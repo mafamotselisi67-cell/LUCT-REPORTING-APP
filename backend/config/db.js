0c020f0170e31030d35bfbcbedadb1dba2f8f6dd
mafamotselisi67-cell <mafamotselisi67@gmail.com>
2025-10-24 16:53:34 +0200

    returned by 'git commit'

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,8 @@
-// const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize');
 
-// const sequelize = new Sequelize('luct_reporting', 'root', 'mosiuoa', {
-//   host: 'localhost',
-//   dialect: 'mysql',
-// });
+const sequelize = new Sequelize('luct_reporting', 'root', 'mosiuoa', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
 
-// module.exports = sequelize;
+module.exports = sequelize;
